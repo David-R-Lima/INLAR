615a3ae1043a1d583559563c8e5ce1abfecd9107
LuLMJ <luciano.martins@univale.br>
2024-10-27 18:17:13 -0300

    +

---
+++ inlar-front-main/src/app/models/enums/doacao-itens/DoacaoItensEvent.ts
@@ -0,0 +1,5 @@
+export enum DoacaoEvent {
+    ADD_DOADOR_ACTION = 'Adicionar DOADOR',
+    EDIT_DOADOR_ACTION = 'Editar DOADOR',
+  }
+  
---
+++ inlar-front-main/src/app/models/enums/doacao/DoacaoEvent.ts
@@ -0,0 +1,4 @@
+export enum DoacaoEvent {
+    ADD_DOACAO_ACTION = 'Adicionar DOACAO',
+    EDIT_DOACAO_ACTION = 'Editar DOACAO',
+  }
