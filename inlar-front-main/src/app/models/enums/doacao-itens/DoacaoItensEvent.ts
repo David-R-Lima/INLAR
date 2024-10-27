2554b501988626deb758d50094deeb0a039c91b8
LuLMJ <luciano.martins@univale.br>
2024-10-27 18:25:52 -0300

    +

--- inlar-front-main/src/app/models/enums/doacao-itens/DoacaoItensEvent.ts
+++ inlar-front-main/src/app/models/enums/doacao-itens/DoacaoItensEvent.ts
@@ -1,5 +1,5 @@
-export enum DoacaoEvent {
-    ADD_DOADOR_ACTION = 'Adicionar DOADOR',
-    EDIT_DOADOR_ACTION = 'Editar DOADOR',
+export enum DoacaoItensEvent {
+    ADD_DOACAO_ITENS_ACTION = 'Adicionar DOACAO ITENS',
+    EDIT_DOACAO_ITENS_ACTION = 'Editar DOACAO ITENS',
   }
   
---
+++ inlar-front-main/src/app/models/enums/tipo-doacao/TipoDoacaoEvent.ts
@@ -0,0 +1,5 @@
+export enum TipoDoacaoEvent {
+    ADD_TIPO_DOACAO_ACTION = 'Adicionar TIPO DOACAO',
+    EDIT_TIPO_DOACAO_ACTION = 'Editar TIPO DOACAO',
+  }
+  
