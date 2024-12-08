42676e736bd04903ad0ec612544bad20afcc90d6
David R Lima <david@deividy.dev>
2024-12-07 22:00:11 -0300

    fix build

--- inlar-front-main/src/environments/environment.prod.ts
+++ inlar-front-main/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: process?.env?.['API_URL'] ?? 'http://localhost:3256',
+  API_URL: 'http://inlar.deividy.dev',
 };
--- inlar-front-main/src/environments/environment.ts
+++ inlar-front-main/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    API_URL: process?.env?.['API_URL'] ?? 'http://localhost:3256'
-}
+  production: false,
+  API_URL: 'http://localhost:3256',
+};
